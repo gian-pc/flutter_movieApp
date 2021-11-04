48f59e465f98cabdcfee096ec05e09cf6a353eb2
Gian Carlos <gpaucarcortez@gmail.com>
2021-11-04 16:37:41 -0500

    Cards movies

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -24,7 +24,6 @@ class _HomePageState extends State<HomePage> {
         "https://api.themoviedb.org/3/discover/movie?api_key=$api_key";
     Uri _uri = Uri.parse(_path);
     http.Response response = await http.get(_uri);
-    print(response.statusCode);
 
     if (response.statusCode == 200) {
       Map<String, dynamic> data = json.decode(response.body);
@@ -133,32 +132,72 @@ class _HomePageState extends State<HomePage> {
                   String urlImage = pathImage + movies[index]["poster_path"];
                   print(urlImage);
 
-                  return Row(
-                    children: [
-                      Container(
-                        width: 100,
-                        height: 100,
-                        margin: EdgeInsets.all(8.0),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(12.0),
-                          image: DecorationImage(
-                            fit: BoxFit.cover,
-                            image: NetworkImage(urlImage),
+                  return Container(
+                    decoration: BoxDecoration(
+                        color: Color(0xff222222),
+                        borderRadius: BorderRadius.circular(12.0),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Color(0xff222222).withOpacity(0.9),
+                            offset: Offset(0,4),
+                            blurRadius: 10.0
+                          )
+                        ]),
+                    margin: EdgeInsets.symmetric(vertical: 8.0),
+                    child: Row(
+                      children: [
+                        Container(
+                          width: 100,
+                          height: 100,
+                          margin: EdgeInsets.all(8.0),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(12.0),
+                            image: DecorationImage(
+                              fit: BoxFit.cover,
+                              image: NetworkImage(urlImage),
+                            ),
                           ),
                         ),
-                      ),
-                      Expanded(
-                        child: Container(
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(movies[index]["original_title"]),
-                              Text(movies[index]["overview"],maxLines: 3,overflow: TextOverflow.ellipsis,)
-                            ],
+                        Expanded(
+                          child: Container(
+                            padding: EdgeInsets.only(right: 8.0, left: 5.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  movies[index]["original_title"],
+                                  style: TextStyle(fontSize: 18.0),
+                                ),
+                                SizedBox(height: 6.0),
+                                Text(
+                                  movies[index]["overview"],
+                                  maxLines: 3,
+                                  overflow: TextOverflow.ellipsis,
+                                  style: TextStyle(
+                                      color: Colors.white.withOpacity(0.6),
+                                      fontSize: 13.0),
+                                ),
+                                SizedBox(height: 4.0),
+                                Row(
+                                  children: [
+                                    Icon(
+                                      Icons.star,
+                                      size: 15.0,
+                                      color: Theme.of(context).accentColor,
+                                    ),
+                                    Text(
+                                      movies[index]["popularity"].toString(),
+                                      style: TextStyle(
+                                          color: Theme.of(context).accentColor),
+                                    ),
+                                  ],
+                                )
+                              ],
+                            ),
                           ),
-                        ),
-                      )
-                    ],
+                        )
+                      ],
+                    ),
                   );
                 },
               )

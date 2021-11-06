fb39b9fc1ceb30583cc2dd9c1755a528ee2be0e3
Gian Carlos <gpaucarcortez@gmail.com>
2021-11-05 22:15:31 -0500

    Detail Page 6

--- lib/pages/detail_movie_page.dart
+++ lib/pages/detail_movie_page.dart
@@ -50,73 +50,121 @@ class _DetailMoviePageState extends State<DetailMoviePage> {
     );
 
     return Scaffold(
-      body: Stack(
-        children: [
-          Container(
-            height: double.infinity,
-            child: Image.network(
-              "https://fondosmil.com/fondo/7351.jpg",
-              fit: BoxFit.fitHeight,
-            ),
-          ),
-          BackdropFilter(
-            filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
-            child: Container(
-              color: Colors.grey.withOpacity(0.1),
-            ),
-          ),
-          CustomScrollView(
-            slivers: [
-              SliverAppBar(
-                title: Text("My Movie"),
-                expandedHeight: 240.0,
-                floating: true,
-                pinned: true,
-                //snap: true,
-                flexibleSpace: FlexibleSpaceBar(
-                  centerTitle: true,
-                  title: Text("Justice League"),
-                  background: Stack(
-                    fit: StackFit.expand,
-                    children: [
-                      Image.network(
-                        "https://fondosmil.com/fondo/7351.jpg",
-                        fit: BoxFit.cover,
-                      ),
-                      DecoratedBox(
-                        decoration: BoxDecoration(
-                            gradient: LinearGradient(
-                          colors: [
-                            Color(0x002E2E2E),
-                            Color(0xff2E2E2E),
+      body: movieDetail != null
+          ? Stack(
+              children: [
+                // Container(
+                //   height: double.infinity,
+                //   child: Image.network(
+                //     "https://fondosmil.com/fondo/7351.jpg",
+                //     fit: BoxFit.fitHeight,
+                //   ),
+                // ),
+                // BackdropFilter(
+                //   filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+                //   child: Container(
+                //     color: Colors.grey.withOpacity(0.1),
+                //   ),
+                // ),
+                CustomScrollView(
+                  slivers: [
+                    SliverAppBar(
+                      title: Text("My Movie"),
+                      expandedHeight: 240.0,
+                      floating: true,
+                      pinned: true,
+                      //snap: true,
+                      flexibleSpace: FlexibleSpaceBar(
+                        centerTitle: true,
+                        title: Text(movieDetail!.originalTitle),
+                        background: Stack(
+                          fit: StackFit.expand,
+                          children: [
+                            Image.network(
+                              pathImage + movieDetail!.backdropPath,
+                              fit: BoxFit.cover,
+                            ),
+                            DecoratedBox(
+                              decoration: BoxDecoration(
+                                  gradient: LinearGradient(
+                                colors: [
+                                  Color(0x002E2E2E),
+                                  Color(0xff2E2E2E),
+                                ],
+                                begin: Alignment.center,
+                                end: Alignment.bottomCenter,
+                              )),
+                            )
                           ],
-                          begin: Alignment.center,
-                          end: Alignment.bottomCenter,
-                        )),
-                      )
-                    ],
-                  ),
-                ),
-              ),
-              SliverList(
-                delegate: SliverChildListDelegate(
-                  [
-                    ListView.builder(
-                      itemCount: 50,
-                      primary: true,
-                      shrinkWrap: true,
-                      physics: ScrollPhysics(),
-                      itemBuilder: (BuildContext context, int index) {
-                        return Text("Hola");
-                      },
+                        ),
+                      ),
+                    ),
+                    SliverList(
+                      delegate: SliverChildListDelegate(
+                        [
+                          Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 12.0),
+                            child: Row(
+                              children: [
+                                Container(
+                                  width: 120.0,
+                                  height: 176.0,
+                                  decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(8.0),
+                                      image: DecorationImage(
+                                          fit: BoxFit.cover,
+                                          image: NetworkImage(pathImage +
+                                              movieDetail!.posterPath))),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.symmetric(horizontal: 12.0),
+                                  child: Column(
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: [
+                                      Text(movieDetail!.originalTitle,
+                                          style: TextStyle(fontSize: 22.0)),
+                                      SizedBox(height: 4.0),
+                                      Text("(${movieDetail!.title})",
+                                          style: TextStyle(fontSize: 12.0)),
+                                      SizedBox(height: 6.0),
+                                      Row(
+                                        children: [
+                                          Icon(Icons.date_range, size: 15,),
+                                          Text(movieDetail!.releaseDate
+                                              .toString()
+                                              .substring(0, 10)),
+                                        ],
+                                      ),
+                                      SizedBox(height: 6.0),
+                                      Row(
+                                        children: [
+                                          Icon(Icons.timer, size: 15),
+                                          Text("${movieDetail!.runtime.toString()} min."),
+                                        ],
+                                      ),
+                                      SizedBox(height: 6.0),
+                                      Row(
+                                        children: [
+                                          Icon(Icons.star, size: 15),
+                                          Text("${movieDetail!.voteAverage.toString()}"),
+                                        ],
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
                   ],
-                ),
-              ),
-            ],
-          )
-        ],
-      ),
+                )
+              ],
+            )
+          : Center(
+              child: CircularProgressIndicator(),
+            ),
     );
   }
 }
